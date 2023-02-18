b7c051c7d9fcf337322fc03a91c63dd5afc37e41
Mandip <4mandip.dev@gmail.com>
2023-02-19 00:43:53 +0530

    latest code

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import HomeStack from './HomeStack';
-import Feather from 'react-native-vector-icons/Feather'
-import ProfileStack from './ProfileStack';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeStack from './HomeStack';
+import Feather from 'react-native-vector-icons/Feather';
+import ProfileStack from './ProfileStack';
+
 import Icon from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Feather from 'react-native-vector-icons/Feather';
 
 import {scale, theme} from '../utils';
 import {Platform, Text, View} from 'react-native';
@@ -25,145 +23,152 @@ const Comman = () => {
 };
 
 const BottomTab = () => {
-    return (
-        <Tab.Navigator screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-
-                if (route.name === 'Home') {
-                    iconName = focused
-                        ? 'calendar'
-                        : 'calendar';
-                } else if (route.name === 'Profile') {
-                    iconName = focused
-                        ? 'user'
-                        : 'user';
-                }
-                return <Feather name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'white',
-            tabBarStyle: {
-                backgroundColor: '#56AB2F',
-            },
-            tabBarLabel: () => { return null },
-        })}>
-            <Tab.Screen name="Home" component={HomeStack} options={{ headerShown: false }} />
-            <Tab.Screen name="Profile" component={ProfileStack} options={{ headerShown: false }} />
-        </Tab.Navigator>
-    )
-}
-
-export default BottomTab
   return (
     <Tab.Navigator
-      // initialRouteName="RISTORANTI"
-      screenOptions={{
-        tabBarHideOnKeyboard: true,
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: theme.colors.purpal,
-        tabBarInactiveTintColor: '#555',
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
 
+          if (route.name === 'Home') {
+            iconName = focused ? 'calendar' : 'calendar';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'user' : 'user';
+          }
+          return <Feather name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
         tabBarStyle: {
-          height: scale(55),
-          position: 'absolute',
-          backgroundColor: theme.colors.primary,
-          paddingTop: Platform.OS === 'ios' ? scale(5) : scale(0),
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 2,
-            height: 2,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-
-          elevation: 5,
+          backgroundColor: '#56AB2F',
         },
-        tabBarLabelStyle: {
-          fontSize: scale(10),
+        tabBarLabel: () => {
+          return null;
         },
-      }}>
+      })}>
       <Tab.Screen
         name="Home"
-        component={Comman}
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <Icon
-                name="calendar"
-                size={scale(25)}
-                color={theme.colors.white}
-              />
-            );
-          },
-        }}
-      />
-
-      <Tab.Screen
-        name="Rupee"
-        component={Comman}
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <FontAwesome
-                name="rupee"
-                size={scale(25)}
-                color={theme.colors.white}
-              />
-            );
-          },
-        }}
-      />
-
-      <Tab.Screen
-        name="Add"
-        component={Comman}
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <Icon
-                name="add-circle-outline"
-                size={scale(25)}
-                color={theme.colors.white}
-              />
-            );
-          },
-        }}
+        component={HomeStack}
+        options={{headerShown: false}}
       />
       <Tab.Screen
-        name="User"
-        component={Profile}
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <Feather
-                name="user"
-                size={scale(25)}
-                color={theme.colors.white}
-              />
-            );
-          },
-        }}
-      />
-
-      <Tab.Screen
-        name="Share"
-        component={Comman}
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <Feather
-                name="share-2"
-                size={scale(25)}
-                color={theme.colors.white}
-              />
-            );
-          },
-        }}
+        name="Profile"
+        component={ProfileStack}
+        options={{headerShown: false}}
       />
     </Tab.Navigator>
   );
 };
 
 export default BottomTab;
+//   return (
+//     <Tab.Navigator
+//       // initialRouteName="RISTORANTI"
+//       screenOptions={{
+//         tabBarHideOnKeyboard: true,
+//         headerShown: false,
+//         tabBarShowLabel: false,
+//         tabBarActiveTintColor: theme.colors.purpal,
+//         tabBarInactiveTintColor: '#555',
+
+//         tabBarStyle: {
+//           height: scale(55),
+//           position: 'absolute',
+//           backgroundColor: theme.colors.primary,
+//           paddingTop: Platform.OS === 'ios' ? scale(5) : scale(0),
+//           shadowColor: '#000',
+//           shadowOffset: {
+//             width: 2,
+//             height: 2,
+//           },
+//           shadowOpacity: 0.25,
+//           shadowRadius: 3.84,
+
+//           elevation: 5,
+//         },
+//         tabBarLabelStyle: {
+//           fontSize: scale(10),
+//         },
+//       }}>
+//       <Tab.Screen
+//         name="Home"
+//         component={Comman}
+//         options={{
+//           tabBarIcon: ({focused}) => {
+//             return (
+//               <Icon
+//                 name="calendar"
+//                 size={scale(25)}
+//                 color={theme.colors.white}
+//               />
+//             );
+//           },
+//         }}
+//       />
+
+//       <Tab.Screen
+//         name="Rupee"
+//         component={Comman}
+//         options={{
+//           tabBarIcon: ({focused}) => {
+//             return (
+//               <FontAwesome
+//                 name="rupee"
+//                 size={scale(25)}
+//                 color={theme.colors.white}
+//               />
+//             );
+//           },
+//         }}
+//       />
+
+//       <Tab.Screen
+//         name="Add"
+//         component={Comman}
+//         options={{
+//           tabBarIcon: ({focused}) => {
+//             return (
+//               <Icon
+//                 name="add-circle-outline"
+//                 size={scale(25)}
+//                 color={theme.colors.white}
+//               />
+//             );
+//           },
+//         }}
+//       />
+//       <Tab.Screen
+//         name="User"
+//         component={Profile}
+//         options={{
+//           tabBarIcon: ({focused}) => {
+//             return (
+//               <Feather
+//                 name="user"
+//                 size={scale(25)}
+//                 color={theme.colors.white}
+//               />
+//             );
+//           },
+//         }}
+//       />
+
+//       <Tab.Screen
+//         name="Share"
+//         component={Comman}
+//         options={{
+//           tabBarIcon: ({focused}) => {
+//             return (
+//               <Feather
+//                 name="share-2"
+//                 size={scale(25)}
+//                 color={theme.colors.white}
+//               />
+//             );
+//           },
+//         }}
+//       />
+//     </Tab.Navigator>
+//   );
+// };
+
+// export default BottomTab;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,4 @@
-
-export { default as HomeScreen } from './home/HomeScreen';
-export { default as ProfileScreen } from './profile/ProfileScreen';
+export {default as HomeScreen} from './home/HomeScreen';
+export {default as ProfileScreen} from './profile/ProfileScreen';
 export {default as SignIn} from './Signin';
 export {default as Profile} from './Profile';
